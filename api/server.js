126f218203e63dcf77931125c02b5b32506a111a
Taylor Picard <taylorkpicard1998@gmail.com>
2023-09-07 22:56:57 -0500

    server set up, working on POST

--- api/server.js
+++ api/server.js
@@ -1,3 +1,32 @@
-// BUILD YOUR SERVER HERE
+// import { insert } from './users/model';
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// "require"
+const express = require('express');
+// "invoke"
+const server = express();
+// "use" (JSON)
+server.use(express.json());
+
+//POST
+server.post('/api/users', async (req, res) => {
+    try {
+        const {id, name, bio } = req.body;
+        if(!id || !name || !bio){
+            res.status(422).json({
+                message: `name and bio required`
+            })
+        }else{
+            
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: `Error creating user`
+        })
+    }
+})
+
+// GET
+server.get('/api/users')
+
+// "export"
+module.exports = server; 
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+// "require"
 const server = require('./api/server');
 
 const port = 9000;
 
 // START YOUR SERVER HERE
+server.listen(port, () => {
+    console.log('server running!')
+})
